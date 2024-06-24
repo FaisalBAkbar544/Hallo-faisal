1042400ce95a51578bd58c032c020d35df245b3c
FProject18 <139376988+FaisalBAkbar544@users.noreply.github.com>
2024-06-25 00:19:12 +0800

    Bismillah BackEndDeveloper 3 digits

---
+++ src/t4kecakapananterpersonal/T4kecakapanAnterpersonal.java
@@ -0,0 +1,15 @@
+
+package t4kecakapananterpersonal;
+
+/**
+ *
+ * @author Muhammad Faisal akbar
+ */
+public class T4kecakapanAnterpersonal {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hallo Faisal");
+    }
+    
+}
